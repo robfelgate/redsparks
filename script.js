4b6690f1034406d7e5857b9a74979f5dd3abdaba
robfelgate <robfelgate@gmail.com>
2019-06-27 17:21:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -164,4 +164,64 @@ $(function () {
 })
 
 
+//gallery jquery
+
+// Isotope for Gallery
+  if ($(".iso-box-wrapper").length > 0) {
+    var $container = $(".iso-box-wrapper"),
+      $imgs = $(".iso-box img");
+
+    $container.imagesLoaded(function() {
+      $container.isotope({
+        layoutMode: "fitRows",
+        itemSelector: ".iso-box"
+      });
+      $imgs.load(function() {
+        $container.isotope("reLayout");
+      });
+    });
+
+    //filter items on button click
+    $(".filter-wrapper li a").click(function() {
+      var $this = $(this),
+        filterValue = $this.attr("data-filter");
+      $container.isotope({
+        filter: filterValue,
+        animationOptions: {
+          duration: 750,
+          easing: "linear",
+          queue: false
+        }
+      });
+
+      // don't proceed if already selected
+      if ($this.hasClass("selected")) {
+        return false;
+      }
+
+      var filter_wrapper = $this.closest(".filter-wrapper");
+      filter_wrapper.find(".selected").removeClass("selected");
+      $this.addClass("selected");
+
+      return false;
+    });
+  }
+
+  // Magnific Popup for Gallery
+  $(".iso-box-wrapper").magnificPopup({
+    //delegate: "a", // child items selector, by clicking on it popup will open
+    fixedContentPos: false,
+    delegate: "a:visible",
+    type: "image",
+     gallery: { enabled: true},
+  zoom: {
+        enabled: true,
+        duration: 300,
+        opener: function (f) {
+
+            return f.find("img");
+        }
+    }
+  });
+
 });
